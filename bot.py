cc2c5722883c3a833d9f01797865d3bfcff1231e
LordRobin1 <geb611018t@gmail.com>
2022-10-05 20:08:44 +0200

    text post support + custom help command

--- bot.py
+++ bot.py
@@ -1,4 +1,3 @@
-# bot.py
 import os
 import discord
 from discord.ext import commands
@@ -11,7 +10,7 @@ def setup():
     TOKEN = os.getenv('DISCORD_TOKEN')    
     intents = discord.Intents.default()
     intents.members = True
-    bot = commands.Bot(command_prefix="?", intents=intents)
+    bot = commands.Bot(command_prefix="?", intents=intents, help_command=None)
     reddit = create_reddit()
     return bot, TOKEN, reddit
 
@@ -19,39 +18,62 @@ bot, TOKEN, r = setup()
 
 
 #commands
-@bot.command(name="hellothere", help="fuck off")
+@bot.command(name="hellothere", help="")
 async def hellothere(ctx):
     print("="*30 + "\ncommand: hellothere\n" + "="*30)
     response = "General Kenobi"
     await ctx.send(response)
 
 
-@bot.command(name="reddit", help="gets a hot image from whatever subreddit you type in")
-async def reddit(ctx, arg, arg2=None):    
-
-    sub = arg    
+@bot.command(name="reddit", help="gets a hot image from provided subreddit `?reddit subreddit info` (`info` optional)")
+async def reddit(ctx, arg=None, arg2=None):
+    sub = arg
+    if sub == None:
+        await ctx.send("Must enter a Subreddit as first argument:\n `?reddit memes`")
+        return
     if arg2 is None:
         print("="*30 + f"\ncommand: reddit ({arg})\n" + "="*30)
-        imgLink = get_images(r, sub)
-        await ctx.send(imgLink)
+        post = await get_images(r, sub)
+        if post['type'] == "gallery": 
+            await ctx.send(f"{post['title']}\n"+
+                           f"{post['url']}"+
+                            "\n*Reddit posts with gallery not supported yet*")
+            return
+        if post['type'] == "text":            
+            try:
+                await sendText(post, ctx)
+            except Exception as e:
+                print(f"error: {e}")
+                await ctx.send("Sorry, an error occured :(")
+            return
+        await ctx.send(f"{post['title']}\n{post['url']}")
+        return
 
-    elif arg2 == "info":
-        print("="*30 + f"\ncommand: reddit ({arg} {arg2})\n" + "="*30)
-        title, desc = sub_info(r, sub)
-        
-        if len(desc) > 1000:
-            desc = "description too long"
+    if arg2 == "info":
+        await sendInfo(sub, ctx)
 
-        embed = discord.Embed(title=title, description=desc, colour=0x87CEEB)
-        await ctx.send(embed=embed)
-
-
-@bot.command(name="test", help="test command")
-async def test(ctx):
-    print("="*30 + "\ncommand: test\n" + "="*30)    
-    embed = discord.Embed(title="Test", description="This is a test for discord.Embed")
+async def sendText(post, ctx):
+    desc = ""
+    if post['text'] != '': desc += post['text']
+    if post['comment'] != '': desc += f"\n\n**Top Comment:**\n{post['comment']}"
+    embed = discord.Embed(title=post['title'], description=desc, colour=0x87ce)
     await ctx.send(embed=embed)
 
+async def sendInfo(sub, ctx):
+    print("="*30 + f"\ncommand: reddit ({sub} info)\n" + "="*30)
+    title, desc = await sub_info(r, sub)        
+    if len(desc) > 4000:
+        desc = "description too long"
+    embed = discord.Embed(title=title, description=desc, colour=0x87CEEB)
+    await ctx.send(embed=embed)
+
+@bot.command(name="help")
+async def help(ctx):
+    title = "Help"
+    desc = "`hellothere`\n\n" + "`help`: Shows this message\n\n" 
+    desc += "`reddit`: gets a hot post from provided subreddit\n`?reddit subreddit info` (`info` optional)"
+    embed = discord.Embed(title=title, description=desc, colour=discord.Colour.from_rgb(26, 188, 156))
+    await ctx.send(embed=embed)
 
 # events
 @bot.event 
--- redditapi.py
+++ redditapi.py
@@ -1,5 +1,5 @@
 import os
-import praw
+import asyncpraw
 import random
 from dotenv import load_dotenv
 
@@ -8,38 +8,65 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 def create_reddit():
-    reddit = praw.Reddit(client_id = CLIENT_ID,
+    reddit = asyncpraw.Reddit(client_id = CLIENT_ID,
                          client_secret = CLIENT_SECRET,
                          user_agent = "api for reddit v1",
                          check_for_async = False)
     return reddit
 
-def get_images(r, sub):
-    
+async def get_images(r, sub):    
     postList = []
-    
-    for submission in r.subreddit(sub).hot():
+    subreddit = await r.subreddit(sub)
+    async for submission in subreddit.hot(limit=50):
         if ".jpg" in submission.url or ".png" in submission.url:
-            postList.append(submission.url)
-        elif ".gif" in submission.url:
+            postList.append({"type": "image", "url": submission.url, "title": submission.title})
+            continue
+        if ".gif" in submission.url:
             tempurl = submission.url.rsplit("?")[0]
             tempurl = tempurl.replace("preview", "i")
-            postList.append(tempurl)
-        elif "v.redd.it" in submission.url:
+            postList.append({"type": "gif", "url": tempurl, "title": submission.title})
+            continue
+        if "v.redd.it" in submission.url:
             tempurl = f"https://www.reddit.com{submission.permalink}?utm_source=share&utm_medium=web2x&context=3"
-            postList.append(tempurl)
-    url = random.choice(postList)        
-    return url   
+            postList.append({"type": "video", "url": tempurl, "title": submission.title})
+            continue
+        if "gallery"in submission.url:
+            submDict = {"type": "gallery", "url": submission.url, "title": submission.title}
+            continue
+        postList.append({"type": "text", "url": submission.url, "name": submission.name, "title": submission.title, "text": submission.selftext})
+
+    submDict = random.choice(postList)
+    print (submDict)
+    if submDict['type'] == "text":
+        submDict['comment'] = ""        
+        name = submDict['name'].split('_')[1]
+        submission.comment_limit = 1
+        submission.comment_sort = 'top'
+        submission = await r.submission(name)        
+        try:
+            comments = submission.comments
+            if(comments):
+                submDict['comment'] = comments[0].body
+                return submDict
+        except Exception as e:
+            print(e)
+    return submDict 
     
 
-def sub_info(r, sub):
-    info = r.subreddit(sub)
+async def sub_info(r, sub):
+    info = await r.subreddit(sub, fetch=True)
     title = f"r/{info.display_name}"
-    id_card = info.widgets.id_card
+    widgets = info.widgets
+    id_card = await widgets.id_card()
 
-    desc =  (f"Subscribers: {info.subscribers} \n" +
-             f"Currently online: {id_card.currentlyViewingCount} \n" + 
-             f"NSFW: {'yes' if info.over18 else 'no'} \n\n" +                
+    cvt = id_card.currentlyViewingText
+    if cvt != 'online' and cvt != '':
+        online = cvt
+    else: online = 'online'
+
+    desc =  (f"Subscribers: {id_card.subscribersCount} {id_card.subscribersText}\n" +
+             f"Currently {id_card.currentlyViewingCount} {online}\n" + 
+             f"NSFW: {'Yes' if info.over18 else 'No'} \n\n" +                
              f"{id_card.description}")
 
     return title, desc
